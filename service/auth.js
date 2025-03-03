1df0b0c3076dae8b80c48f43d1402d4908c8727d
Anshika Gangwar <anshikagangwar341@gmail.com>
2025-03-03 13:23:14 +0530

    JWT Authentication

--- service/auth.js
+++ service/auth.js
@@ -1,14 +1,20 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken")
+const secret= "Anshika$123@g"
 
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user)
+function setUser(user){
+   return jwt.sign({
+    _id: user._id,
+    email: user.email,
+   },secret)
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id)
+function getUser(token){
+    if(!token) return null;
+    return jwt.verify(token,secret)
 }
 
 module.exports= {
     setUser,
     getUser,
 }
+
